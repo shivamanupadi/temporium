267db331b965aef89b118f8b76a3e2b2b213beae
shivamanupadi <137275428+shivamanupadi@users.noreply.github.com>
2025-12-28 12:34:51 +0530

    bug fixes

--- apps/gateway/api/drizzle.config.ts
+++ apps/gateway/api/drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   out: './src/db/migrations',
   dialect: 'sqlite',
   dbCredentials: {
-    url: '.wrangler/state/v3/d1/miniflare-D1DatabaseObject/placeholder.sqlite',
+    url: '.wrangler/state/v3/d1/miniflare-D1DatabaseObject/4158b66db26db07bc157feda61f1d3542345752d8baeb2cbe0e2f8808e34815d.sqlite',
   },
 });
